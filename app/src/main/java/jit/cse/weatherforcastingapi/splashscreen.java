04fde16ccf99addf87be81c009ad13f579f49145
esaikrishna28 <esaikrishna28@gmail.com>
2023-06-14 22:28:59 +0530

    final without recycler view and with splash screen

--- app/src/main/java/jit/cse/weatherforcastingapi/splashscreen.java
+++ app/src/main/java/jit/cse/weatherforcastingapi/splashscreen.java
@@ -2,7 +2,9 @@ package jit.cse.weatherforcastingapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class splashscreen extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class splashscreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // on below line we are
+                // creating a new intent
+                Intent i = new Intent(splashscreen.this, MainActivity.class);
+
+                // on below line we are
+                // starting a new activity.
+                startActivity(i);
+
+                // on the below line we are finishing
+                // our current activity.
+                finish();
+            }
+        }, 4000);
     }
 }
